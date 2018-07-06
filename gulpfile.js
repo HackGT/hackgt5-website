955ca6de43a4393e2661732c4e90f6de009af548
Kexin Zhang <kxin.zhang@gmail.com>
2018-07-06 02:14:39 -0700

    add more basic content, include nav in each subsection

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 var plumber = require('gulp-plumber');
+var fileinclude = require('gulp-file-include');
 
 var pathPrefix = "src/";
 var paths = ["header.html", "pages/*.html", "footer.html"];
@@ -14,6 +15,10 @@ function build() {
     console.log("Building site...");
     paths = paths.map(path => pathPrefix + path);
     return gulp.src(paths)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
         .pipe(plumber())
         .pipe(concat("index.html"))
         .pipe(gulp.dest("dist/"));
