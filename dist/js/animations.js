9904f7d206ad5966a5bf5a1fcfa05fc393d4ee6c
Kexin Zhang <kxin.zhang@gmail.com>
2018-07-08 12:54:29 -0700

    tweak switching colors of navigational elements

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -4,15 +4,14 @@ new fullpage('#fullpage', {
     anchors: ['splash', 'event', 'tracks', 'schedule', 'faq', 'registration', 'about', 'sponsors'],
     menu: '#nav',
     onLeave: function(origin, destination, direction) {
-        if (destination.index === 2 || destination.index === 4 || destination.index === 6) {
+        var dark_text_pages = [2, 4, 6];
+        if (dark_text_pages.indexOf(destination.index) != -1) {
             document.getElementById('scroll-nav').classList.add('dark-text');
             document.getElementById('nav').classList.add('dark-text');
             document.querySelectorAll('#social-media path').forEach(function(elem) {
                 elem.setAttribute('fill', '#0c0c51');
             });
-        }
-
-        if (destination.index === 1 || destination.index === 3 || destination.index === 5 || destination.index === 7) {
+        } else {
             document.getElementById('scroll-nav').classList.remove('dark-text');
             document.getElementById('nav').classList.remove('dark-text');
             document.querySelectorAll('#social-media path').forEach(function(elem) {
