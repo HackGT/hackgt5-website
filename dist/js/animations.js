eacc26b0ed9b5bcae98a6407ed2d6ee2ad6749d6
Kexin Zhang <kxin.zhang@gmail.com>
2018-07-08 08:51:59 -0700

    fix positioning overlay issues

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -6,7 +6,7 @@ new fullpage('#fullpage', {
         if (destination.index === 2 || destination.index === 4 || destination.index === 6) {
             document.getElementById('scroll-nav').classList.add('dark-text');
             document.getElementById('nav').classList.add('dark-text');
-            document.querySelectorAll('.social-media path').forEach(function(elem) {
+            document.querySelectorAll('#social-media path').forEach(function(elem) {
                 elem.setAttribute('fill', '#0c0c51');
             });
         }
@@ -14,7 +14,7 @@ new fullpage('#fullpage', {
         if (destination.index === 1 || destination.index === 3 || destination.index === 5 || destination.index === 7) {
             document.getElementById('scroll-nav').classList.remove('dark-text');
             document.getElementById('nav').classList.remove('dark-text');
-            document.querySelectorAll('.social-media path').forEach(function(elem) {
+            document.querySelectorAll('#social-media path').forEach(function(elem) {
                 elem.setAttribute('fill', '#fff');
             })
         }
