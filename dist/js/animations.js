58ed89624c168a1802fd5911c2a38cda121bc57c
Kexin Zhang <kxin.zhang@gmail.com>
2018-07-07 21:33:33 -0700

    start on scroll behavior

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -1,1 +1,16 @@
-// animation stuff here
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    onLeave: function(origin, destination, direction) {
+        if (destination.index === 2 || destination.index === 4 || destination.index === 6) {
+            document.getElementById('scroll-nav').classList.add('dark-text');
+            document.getElementById('nav').classList.add('dark-text');
+        }
+
+        if (destination.index === 1 || destination.index === 3 || destination.index === 7) {
+            document.getElementById('scroll-nav').classList.remove('dark-text');
+            document.getElementById('nav').classList.remove('dark-text');
+        }
+    }
+}); 
